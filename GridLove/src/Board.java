8c5aa35ba90bbfe3121bf713e363f856f25bb269
xzx666666 <z5125746@unsw.edu.au>
2018-05-06 16:38:32 +1000

    project

---
+++ GridLove/src/Board.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Board {
+	private ArrayList<ArrayList<Vechicle>> board;
+	
+	public boolean checkLeft () {
+		
+	}
+	public boolean checkRight() {
+		
+	}
+	public boolean checkUp() {
+		
+	}
+	public boolean checkDown() {
+		
+	}
+	public void addVehicle () {
+		
+	}
+	
+	public boolean moveLeft(Vehicle v) {
+		
+		//check left
+		
+		
+		//set address
+		
+		
+		
+	}
+}
---
+++ GridLove/src/Vehicle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Vehicle {
+	private int orientation; // 0 - horizontal, 1-vertical
+	private ArrayList<ArrayList<Integer>> address;
+	
+	public boolean setAddress () {
+		
+	}
+	public int getVehicleOrientation() {
+		return this.orientation;
+	}
+}
