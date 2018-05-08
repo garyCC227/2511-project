e5573d7b880ee4f78962466c05c1d9a3458debe0
Enda Shi <giganto132@hotmail.com>
2018-05-08 14:33:46 +1000

    Vehicle: Reformatted code for clarity Added setAddress
    Board:
    Reformatted code for clarity
    Added addVehicle

--- GridLove/src/Board.java
+++ GridLove/src/Board.java
@@ -1,331 +1,311 @@
 import java.util.*;
 
 public class Board {
-	private Vehicle[][] board;
-	// add Arraylist of vehicles
+    private Vehicle[][] board;
+    private ArrayList<Vehicle> vehicleList;
+    private static final int VERTICAL = 1;
+    private static final int HORIZONTAL = 0;
+    private static final int CAR = 2;
+    private static final int TRUCK = 3;
+    private static final int TAIL = 1;
+    private static final int HEAD = 0;
+    private static final int Y_COORD = 1;
+    private static final int X_COORD = 0;
+    // add Arraylist of vehicles
 
+    public Board() {
+        this.board = new Vehicle[6][6];
+        this.vehicleList = new ArrayList<Vehicle>();
+    }
 
-	public Board() {
-		this.board = new Vehicle[6][6];
-        //how about the size 6 for Vechicle[5][5]?
+    public Vehicle[][] getBoard() {
+        return board;
+    }
 
-	}
-
-	public Vehicle[][] get_board() {
-		return board;
-	}
-
-	// ======================= all check operation
-	/*
-	 * 0 - horizontal, 1-vertical
-	 * for if it is a valid Up operation
-	 * check available space in board? check valid orientation?
-	 */
-	public boolean checkUp(Vehicle v) {
-		//check orientation is valid for this vehicle ?
-		if(v.get_ori() != 1) {
-			return false;
-		}
-
-		/*
-		 * check is there available space in board to allow vehicle move?
-		 * for Up operation, we need to check head of the vehicle
-		 * no valid situation:
-		 * - vehicle locates at top of board
-		 * - not available space in board to move in
-		 */
-		int[][] tempAddress = v.get_address();
-		int headX = tempAddress[0][0];
-		int headY = tempAddress[0][1];
-		if(headY == 0 || board[headX][headY-1] != null) {
-			return false;
-		}
-
-		return true;
-	}
+    // ======================= all check operation
     /*
-     * 0 - horizontal, 1-vertical
-     * for if it is a valid left operation
-     * check available space in board? check valid orientation?
+     * 0 - horizontal, 1-vertical for if it is a valid Up operation check available
+     * space in board? check valid orientation?
      */
-    public boolean checkLeft(Vehicle v) {
-        //check orientation is valid for this vehicle ?
-        if(v.get_ori() != 0) {
+    public boolean canMoveUp(Vehicle v) {
+        // check orientation is valid for this vehicle ?
+        if (v.getOrientation() != VERTICAL) {
             return false;
         }
-        
+
         /*
-         * check is there available space in board to allow vehicle move?
-         * for Up operation, we need to check head of the vehicle
-         * no valid situation:
-         * - vehicle locates at top of board
-         * - not available space in board to move in
+         * check is there available space in board to allow vehicle move? for Up
+         * operation, we need to check head of the vehicle no valid situation: - vehicle
+         * locates at top of board - not available space in board to move in
          */
-        int[][] tempAddress = v.get_address();
-        int headX = tempAddress[0][0];
-        int headY = tempAddress[0][1];
-        if(headX == 0 || board[headX-1][headY] != null) {
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+
+        if (headY == 0 || board[headX][headY - 1] != null) {
             return false;
         }
-        
+
         return true;
     }
+
     /*
-     * 0 - horizontal, 1-vertical
-     * for if it is a valid Down operation
-     * check available space in board? check valid orientation?
+     * 0 - horizontal, 1-vertical for if it is a valid left operation check
+     * available space in board? check valid orientation?
      */
-    public boolean checkDown(Vehicle v){
-        int tailX,tailY;
-        if(v.get_ori() != 1){
+    public boolean canMoveLeft(Vehicle v) {
+        // check orientation is valid for this vehicle 
+        if (v.getOrientation() != 0) {
             return false;
         }
+
         /*
-         * check is there available space in board to allow vehicle move?
-         * for Up operation, we need to check head of the vehicle
-         * no valid situation:
-         * - vehicle locates at top of board
-         * - not available space in board to move in
+         * check is there available space in board to allow vehicle move? for Up
+         * operation, we need to check head of the vehicle no valid situation: - vehicle
+         * locates at top of board - not available space in board to move in
          */
-        
-        int [][] tempAddress = v.get_address();
-        if(v.get_size==2){
-            tailX = tempAddress[1][0];
-            tailY = tempAddress[1][1];
-            
-        }
-        else if(v.get_size==3){
-            tailX = tempAddress[2][0];
-            tailY = tempAddress[2][1];
-        }
-        if(tailY == 5|| board[tailX][tailY+1] != null){
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+
+        if (headX == 0 || board[headX - 1][headY] != null) {
             return false;
         }
+
         return true;
-        
     }
+
     /*
-     * 0 - horizontal, 1-vertical
-     * for if it is a valid Right operation
-     * check available space in board? check valid orientation?
+     * 0 - horizontal, 1-vertical for if it is a valid Down operation check
+     * available space in board? check valid orientation?
      */
-    public boolean checkRight(Vehicle v){
-        int tailX,tailY;
-        if(v.get_ori() != 0){
+    public boolean canMoveDown(Vehicle v) {
+        // check orientation for vehicle
+        if (v.getOrientation() != 1) {
             return false;
         }
         /*
-         * check is there available space in board to allow vehicle move?
-         * for Up operation, we need to check head of the vehicle
-         * no valid situation:
-         * - vehicle locates at top of board
-         * - not available space in board to move in
+         * check is there available space in board to allow vehicle move? for Up
+         * operation, we need to check head of the vehicle no valid situation: - vehicle
+         * locates at top of board - not available space in board to move in
          */
-        int [][] tempAddress = v.get_address();
-        if(v.get_size==2){
-            tailX = tempAddress[1][0];
-            tailY = tempAddress[1][1];
-            
-        }
-        else if(v.get_size==3){
-            tailX = tempAddress[2][0];
-            tailY = tempAddress[2][1];
-        }
-        if(tailX == 5|| board[tailX+1][tailY] != null){
+        int[][] currAddress = v.getAddress();
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        if (tailY == 5 || board[tailX][tailY + 1] != null) {
             return false;
         }
         return true;
-        
+
     }
 
-	//add more check..
-
-	// =============================== all move operation
-	/*
-	 * move vehicle one step up,
-	 * update board, update vehicle in collection of vehicles
-	 * @pre: must call checkUp() first.
-	 *
-	 */
-	public void moveUp(Vehicle v) {
-		int[][] tempAddress = v.get_address();
-		int headX = tempAddress[0][0], headY = tempAddress[0][1];
-		int tailX, tailY;
-
-		//vehicle is car
-		if(v.get_size() == 2) {
-			tailX = tempAddress[1][0];
-			tailY = tempAddress[1][1];
-		}else if(v.get_size() == 3) {
-		//vehicle is truck
-			tailX = tempAddress[2][0];
-			tailY = tempAddress[2][1];
-		}else {
-			System.out.println("Erro: invalid size");
-			return;
-		}
-
-		//update board
-		board[headX][headY-1] = v;
-		board[tailX][tailY] = null;
-
-		//update Vehicle
-		if(v.get_size() == 2) {
-			tempAddress[0][1] -=1; // headY move up 1
-			tempAddress[1][1] -=1; // tailY move up 1
-		}else if(v.get_size() == 3) {
-			tempAddress[0][1] -= 1; //headY move up 1
-			tempAddress[1][1] -= 1;	//bodyY move up 1
-			tempAddress[2][1] -= 1; //tailY move up 1
-		}
-	}
     /*
-     * move vehicle one step Left,
-     * update board, update vehicle in collection of vehicles
-     * @pre: must call checkLeft() first.
+     * 0 - horizontal, 1-vertical for if it is a valid Right operation check
+     * available space in board? check valid orientation?
+     */
+    public boolean canMoveRight(Vehicle v) {
+        if (v.getOrientation() != 0) {
+            return false;
+        }
+        /*
+         * check is there available space in board to allow vehicle move? for Up
+         * operation, we need to check head of the vehicle no valid situation: - vehicle
+         * locates at top of board - not available space in board to move in
+         */
+        int[][] currAddress = v.getAddress();
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        if (tailX == 5 || board[tailX + 1][tailY] != null) {
+            return false;
+        }
+        return true;
+
+    }
+
+    // add more check..
+
+    // =============================== all move operation
+    /*
+     * move vehicle one step up, update board, update vehicle in collection of
+     * vehicles
+     * 
+     * @pre: must call canMoveUp() first.
      *
      */
-    public void moveLeft(Vehicle v) {
-        int[][] tempAddress = v.get_address();
-        int headX = tempAddress[0][0], headY = tempAddress[0][1];
-        int tailX, tailY;
-        
-        //vehicle is car
-        if(v.get_size() == 2) {
-            tailX = tempAddress[1][0];
-            tailY = tempAddress[1][1];
-        }else if(v.get_size() == 3) {
-            //vehicle is truck
-            tailX = tempAddress[2][0];
-            tailY = tempAddress[2][1];
-        }else {
-            System.out.println("Erro: invalid size");
-            return;
+    public boolean moveUp(Vehicle v) {
+        if (!v.canMoveUp()) {
+            return false;
         }
         
-        //update board
-        board[headX-1][headY] = v;
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        // update board
         board[tailX][tailY] = null;
+        board[headX][headY - 1] = v;
+
+        // update Vehicle
+        currAddress[HEAD][Y_COORD]--;
+        currAddress[TAIL][Y_COORD]--;
         
-        //update Vehicle
-        if(v.get_size() == 2) {
-            tempAddress[0][0] -=1; // headX move left 1
-            tempAddress[1][0] -=1; // tailX move left 1
-        }else if(v.get_size() == 3) {
-            tempAddress[0][0] -= 1; //headX move left 1
-            tempAddress[1][0] -= 1;    //bodyX move left 1
-            tempAddress[2][0] -= 1; //tailX move left 1
-        }
+        return true;
     }
+
     /*
-     * move vehicle one step Right,
-     * update board, update vehicle in collection of vehicles
-     * @pre: must call checkRight() first.
+     * move vehicle one step Left, update board, update vehicle in collection of
+     * vehicles
+     * 
+     * @pre: must call canMoveLeft() first.
      *
      */
-    public void moveRight(Vehicle v) {
-        int[][] tempAddress = v.get_address();
-        int headX = tempAddress[0][0], headY = tempAddress[0][1];
-        int tailX, tailY;
-        
-        //vehicle is car
-        if(v.get_size() == 2) {
-            tailX = tempAddress[1][0];
-            tailY = tempAddress[1][1];
-        }else if(v.get_size() == 3) {
-            //vehicle is truck
-            tailX = tempAddress[2][0];
-            tailY = tempAddress[2][1];
-        }else {
-            System.out.println("Erro: invalid size");
-            return;
+    public boolean moveLeft(Vehicle v) {
+        if (!v.canMoveLeft()) {
+            return false;
         }
         
-        //update board
-        board[headX+1][headY] = v;
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        // update board
         board[tailX][tailY] = null;
+        board[headX - 1][headY] = v;
+
+        // update Vehicle
+        currAddress[HEAD][X_COORD]--;
+        currAddress[TAIL][X_COORD]--;
         
-        //update Vehicle
-        if(v.get_size() == 2) {
-            tempAddress[0][0] +=1; // headX move right 1
-            tempAddress[1][0] +=1; // tailX move right 1
-        }else if(v.get_size() == 3) {
-            tempAddress[0][0] += 1; //headX move right 1
-            tempAddress[1][0] += 1;    //bodyX move right 1
-            tempAddress[2][0] += 1; //tailX move right 1
-        }
+        return true;
     }
+
     /*
-     * move vehicle one step Down,
-     * update board, update vehicle in collection of vehicles
-     * @pre: must call checkDown() first.
+     * move vehicle one step Right, update board, update vehicle in collection of
+     * vehicles
+     * 
+     * @pre: must call canMoveRight() first.
+     *
+     */
+    public boolean moveRight(Vehicle v) {
+        if (!v.canMoveRight()) {
+            return false;
+        }
+        
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        // update board
+        board[tailX + 1][tailY] = v;
+        board[headX][headY] = null;
+
+        // update Vehicle
+        currAddress[HEAD][X_COORD]++;
+        currAddress[TAIL][X_COORD]++;
+        
+        return true;
+    }
+
+    /*
+     * move vehicle one step Down, update board, update vehicle in collection of
+     * vehicles
+     * 
+     * @pre: must call canMoveDown() first.
      *
      */
     public void moveDown(Vehicle v) {
-        int[][] tempAddress = v.get_address();
-        int headX = tempAddress[0][0], headY = tempAddress[0][1];
-        int tailX, tailY;
-        
-        //vehicle is car
-        if(v.get_size() == 2) {
-            tailX = tempAddress[1][0];
-            tailY = tempAddress[1][1];
-        }else if(v.get_size() == 3) {
-            //vehicle is truck
-            tailX = tempAddress[2][0];
-            tailY = tempAddress[2][1];
-        }else {
-            System.out.println("Erro: invalid size");
-            return;
+        if (!v.canMoveDown()) {
+            return false;
         }
         
-        //update board
-        board[headX][headY+1] = v;
-        board[tailX][tailY] = null;
+        int[][] currAddress = v.getAddress();
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
         
-        //update Vehicle
-        if(v.get_size() == 2) {
-            tempAddress[0][1] +=1; // headY move down 1
-            tempAddress[1][1] +=1; // tailY move down 1
-        }else if(v.get_size() == 3) {
-            tempAddress[0][1] += 1; //headY move down 1
-            tempAddress[1][1] += 1;    //bodyY move down 1
-            tempAddress[2][1] += 1; //tailY move down 1
+        // update board
+        board[tailX][tailY + 1] = v;
+        board[headX][headY] = null;
+
+        // update Vehicle
+        currAddress[HEAD][Y_COORD]++;
+        currAddress[TAIL][Y_COORD]++;
+        
+        return true;
+    }
+    
+    /* adds vehicle to board
+     * @pre vehicle is not null, vehicle contains valid address
+     */
+    public boolean addVehicle(Vehicle v) {
+        int headX = currAddress[HEAD][X_COORD], 
+        int headY = currAddress[HEAD][Y_COORD];
+        int tailX = currAddress[TAIL][X_COORD];
+        int tailY = currAddress[TAIL][Y_COORD];
+        
+        // checks if there is available space on the board
+        for (int i = headX; i <= tailX; i++) {
+            for (int j = headY; j <= tailY; j++) {
+                if (board[i][j] != null) {
+                    // if vehicle address already taken, return false
+                    return false;
+                }
+            }
         }
+                
+        // adds vehicle to list of vehicles in board
+        vehicleList.add(v);
+        
+        return true;
     }
 
-	public void addVehicle () {
+    /*
+     * input info: which vehicle, what movement user take movement operation: grab
+     * input information and send them to relative methods.
+     * 
+     * @param: vehicle, movement
+     * 
+     * @return: return true success, otherwise not valid operation
+     */
+    public boolean movementOp(Vehicle v, String move) {
+        switch (move) {
+        case "Up":
+            // check if it is a valid operation
+            if (!canMoveUp(v)) {
+                return false;
+            }
 
-	}
-
-	/*
-	 * input info:  which vehicle,   what movement user take
-	 * movement operation: grab input information and send them to relative methods.
-	 * @param:  vehicle, movement
-	 * @return: return true success, otherwise not valid operation
-	 */
-	public boolean movementOp(Vehicle v, String move) {
-		switch (move) {
-		case "Up":
-			//check if it is a valid operation
-			if(!checkUp(v)) { return false; }
-
-			//add more staff for update ...
-			break;
-		case "Down":
-             if(!checkDown(v)) { return false; }
+            // add more staff for update ...
             break;
-		case "Left":
-             if(!checkLeft(v)) { return false; }
+        case "Down":
+            if (!canMoveDown(v)) {
+                return false;
+            }
             break;
-		case "Right":
-              if(!checkRight(v)) { return false; }
+        case "Left":
+            if (!canMoveLeft(v)) {
+                return false;
+            }
             break;
-		default:
-			System.out.println("Errno: Invalid input -> " + move);
-			return false;
-		}
+        case "Right":
+            if (!canMoveRight(v)) {
+                return false;
+            }
+            break;
+        default:
+            System.out.println("Errno: Invalid input -> " + move);
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
--- GridLove/src/Vehicle.java
+++ GridLove/src/Vehicle.java
@@ -1,41 +1,35 @@
 import java.util.*;
 
 public class Vehicle {
-	private final int orientation; // 0 - horizontal, 1-vertical
-	private int[][] address; // 2d array represent its head/tail x,y coordinate
-	private int size;		// size of vehicle
+    private int orientation; // 0 - horizontal, 1-vertical
+    private int[][] address; // 2d array represent its head/tail x,y coordinate
+    private int size; // size of vehicle
 
-	public Vehicle(final int orientation, int size) {
-		this.orientation = orientation;
-		this.size = size;
-		init_address(this.size);
+    public Vehicle(int orientation, int size) {
+        this.orientation = orientation;
+        this.size = size;
+        this.address = new int[2][2];
+    }
 
-	}
+    public int getSize() {
+        return this.size;
+    }
 
-	/*
-	 *@pre: must be called after size is initialized, only use it when constructor called
-	 * initialize a 2-D array for store address of vehicle
-	 * e.g.  for a vertical car
-	 * {5,5        -----head x == [0][0], head y == [0][1]
-	 *  5,6} 	   -----tail x == [1][0], tail y == [1][1]
-	 */
-	public void init_address(int size) {
-		this.address = new int[2][size];
-	}
+    public int getOrientation() {
+        return this.orientation;
+    }
 
-	public int get_size() {
-		return this.size;
-	}
+    public int[][] getAddress() {
+        return this.address;
+    }
+    
+    public void setAddress(int[][] address) {
+        // copies address
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                this.address[i][j] = address[i][j];
+            }
+        }
+    }
 
-	public final int get_ori() {
-		return this.orientation;
-	}
-
-	public int[][] get_address(){
-		return this.address;
-	}
-
-	public int getVehicleOrientation() {
-		return this.orientation;
-	}
 }
